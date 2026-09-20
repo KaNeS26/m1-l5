8fc412ce017854538abc8cec2afa601cba6da5ef
KaNeS26 <caneschihenrique@gmail.com>
2026-09-20 10:13:39 -0300

    Initialize Discord bot with image command
    Set up a Discord bot with a command to send random images.

--- a.py
+++ a.py
@@ -1,1 +1,28 @@
+import discord
+from discord.ext import commands
+import os, random
+from dotenv import load_dotenv
 
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Você logou como {bot.user}')
+
+@bot.command()
+async def mem(ctx):
+    a = ["mem1.png","mem2.png","mem3.png","mem4.png"]
+    chance = [10, 30, 50, 10]
+
+    img_name = random.choices(a, weights=chance, k=1)[0]
+    with open(f'h/{img_name}', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
+bot.run(TOKEN)
